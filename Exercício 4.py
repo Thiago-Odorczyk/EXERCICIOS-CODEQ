6da726769c982864f44d8e7f18b6445d17173ed6
Thiago-Odorczyk <131110994+Thiago-Odorczyk@users.noreply.github.com>
2023-04-27 18:03:33 -0300

    Add files via upload
    Exercício 4

---
+++ Exercício 4.py
@@ -0,0 +1,14 @@
+A = float(input("Digite o valor de A "))
+B = float(input("Digite o valor de B "))
+C = float(input("Digite o valor de C "))
+PI = 3.14159
+Triangulo = (A*C)/2
+print("Triângulo:", round(Triangulo, 3))
+Circulo = PI*C**2
+print("Círculo:", round(Circulo, 3))
+Trapezio = ((A+B)*C)/2
+print("Trapézio:", round(Trapezio, 3))
+Quadrado = B*B
+print("Quadrado:", round(Quadrado, 3))
+Retangulo = A*B
+print("Retângulo:", round(Retangulo, 3))
