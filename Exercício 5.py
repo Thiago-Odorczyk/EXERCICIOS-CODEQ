19c593d9d98f0abd08617b4868c838919f7dc031
Thiago-Odorczyk <131110994+Thiago-Odorczyk@users.noreply.github.com>
2023-04-27 18:04:07 -0300

    Add files via upload
    Exercício 5

---
+++ Exercício 5.py
@@ -0,0 +1,6 @@
+x1 = float(input("informe o valor de x do primeiro ponto "))
+y1 = float(input("informe o valor de y do primeiro ponto "))
+x2 = float(input("informe o valor de x do segundo ponto "))
+y2 = float(input("informe o valor de y do segundo ponto "))
+distancia = ((x1-x2)**2 + (y1-y2)**2)**0.5
+print("A distÂncia entre os pontos é:", round(distancia, 4))
