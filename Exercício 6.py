7f30529c4b2a502307c5890d72f2ef3a7ac4d9e3
Thiago-Odorczyk <131110994+Thiago-Odorczyk@users.noreply.github.com>
2023-04-27 18:04:30 -0300

    Add files via upload
    Exercício 6

---
+++ Exercício 6.py
@@ -0,0 +1,6 @@
+numerofuncionario = int(input("Digite o número do funcionário "))
+numerodehoras = int(input("Digite o número de horas trabalhadas "))
+valordahora = float(input("Digite o valor da hora trabalhada "))
+salario = numerodehoras*valordahora
+print("NUMBER =", numerofuncionario)
+print("SALARY = U$", round(salario, 2))
