d93472b12473bd0d195b87c86d5ff2fcf9a72d26
Thiago-Odorczyk <131110994+Thiago-Odorczyk@users.noreply.github.com>
2023-04-29 12:14:08 -0300

    Add files via upload
    Exercício 9

---
+++ Exercício 9.py
@@ -0,0 +1,21 @@
+DDD = int(input("Digite o número do DDD "))
+if DDD == 61:
+   print ("Este DDD pertence a cidade de Brasília")
+if DDD == 71:
+   print ("Este DDD pertence a cidade de Salvador")
+if DDD == 11:
+   print ("Este DDD pertence a cidade de São Paulo")
+if DDD == 21:
+   print ("Este DDD pertence a cidade de Rio de Janeiro")
+if DDD == 32:
+   print ("Este DDD pertence a cidade de Juiz de Fora")
+if DDD == 19:
+   print ("Este DDD pertence a cidade de Campinas")
+if DDD == 27:
+   print ("Este DDD pertence a cidade de Vitória")
+if DDD == 31:
+   print ("Este DDD pertence a cidade de Belo Horizonte")
+if DDD == 55:
+   print ("Este DDD pertence a cidade de Santa Maria")
+else:
+    print("DDD não cadastrado")
