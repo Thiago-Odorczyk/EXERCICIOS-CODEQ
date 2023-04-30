3cb750038881cac14d47c48036db17f703fb2ce0
Thiago-Odorczyk <131110994+Thiago-Odorczyk@users.noreply.github.com>
2023-04-30 01:21:21 -0300

    Add files via upload
    Exercício 15

---
+++ Exercício 15.py
@@ -0,0 +1,6 @@
+print ("Você pode pressionar os botões: 0 1 2 3 4 5")
+nomes_faixas = ["PROXYCITY","P.Y.N.G.","DNSUEY!","SERVERS","HOST!","CRIPTONIZE","OFFLINE DAY","SALT","ANSWER!","RAR?","WIFI ANTENNAS"]
+X = int(input("Presione o primeiro botão "))
+Y = int(input("Presione o segundo botão "))
+C = X + Y
+print (nomes_faixas[C],"♪♫♪♫")
